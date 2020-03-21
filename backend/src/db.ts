46b24798203974951b5bf20e7110fc4dd969dddb
Robi Hammerle <robi.hammerle@three10.de>
2020-03-21 21:47:13 +0100

    refactor: Code cleanup

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -5,11 +5,11 @@ export type Media = {
 	id: string;
 	name: string;
 	uploadTime: number;
-	connectedTags: Tag[];
+	connectedTags?: Tag[];
 };
 export type Tag = {
 	id: string;
-	// 	medias:Media[];
+	medias?:Media[];
 };
 
 
@@ -38,14 +38,42 @@ class DataBase {
 
 	}
 
+// Tag Functions
+
 	public async AddTag(tag: Tag): Promise < any > {
 		(await this.database).get('Tags', []).push(tag).value();
 		(await this.database).write();
 	}
 
+	public async GetTag(tagId:string):Promise<Tag>{
+		const tag = (await this.database).get('Tags')
+					.find({id:tagId})
+					.value();
+		if(!tag)
+			throw new Error("Tag with ID "+tagId+" not found");
+		return tag;
+	}
+
+	public async UpdateTag(tag:Tag):Promise <any>{
+		(await this.database).get('Tags')
+		.find({id : tag.id})
+		.assign(tag)
+		.write()
+	}
+
+// Media Functions
 	public async AddMedia(media:Media):Promise <any>{
 		(await this.database).get('Medias', []).push(media).value();
 		(await this.database).write();
+	}
+
+	public async GetMedid(mediaId:string):Promise<Media>{
+		const media = (await this.database).get('Medias', [])
+						.find({id:mediaId})
+						.value();
+		if(!media)
+			throw new Error("Media with ID "+mediaId+" not found");
+		return media;
 	}
 
 	public async MediaExists(filename:string):Promise <boolean>{
--- backend/src/routes/uploadRoutes.ts
+++ backend/src/routes/uploadRoutes.ts
@@ -1,33 +1,32 @@
 import express, {Request,Response,NextFunction} from 'express';
 import multer from 'multer';
 import app from '../app';
-import {Tag,Media, DataBase} from '../db';
+import {Media} from '../db';
 import sanitize from 'sanitize-filename';
 import shortid from 'shortid';
 
 const router = express.Router();
-const storage = multer.diskStorage(
-	{destination(req, file, cb) {cb(null, './uploads/')},
-	filename(req: Express.Request, file:Express.Multer.File, cb: any) {
+const storage = multer.diskStorage({
+	destination(req, file, cb) {
+		cb(null, './uploads/')
+	},
+	filename(req: Express.Request, file: Express.Multer.File, cb: any) {
 		const cleanFilename = sanitize(file.originalname);
 		cb(null, cleanFilename)
 	}
 });
-const fileFilter = (req: Express.Request, file:Express.Multer.File, cb: any) => {
-	if (file.mimetype === "image/jpg" ||
-		file.mimetype === "image/jpeg" ||
-		file.mimetype === "image/png") {
-
+const fileFilter = (req: Express.Request, file: Express.Multer.File, cb: any) => {
+	if (file.mimetype === "video/mp4 ") {
 		const exists = app.db.MediaExists(file.originalname);
 		exists.then(value => {
 			console.log('resolved', value);
-		  });
-		  exists.catch(error => {
+		});
+		exists.catch(error => {
 			console.log('rejected', error);
-		  });
-		  cb(null,true);
+		});
+		cb(null, true);
 	} else {
-		cb(new Error("Image uploaded is not of type jpg/jpeg or png"), false);
+		cb(new Error("Currently only MP4 video files are supported"), false);
 	}
 }
 
@@ -36,15 +35,16 @@ const upload = multer({
 	fileFilter
 });
 
-router.post('/', upload.array('images', 5), async (req: Request,
+router.post('/', upload.array('files', 5), async (req: Request,
 	res: Response, next: NextFunction) => {
 	const files = req.files as Express.Multer.File[];
 	files.forEach(file => {
-		const media:Media =
-		{id:shortid.generate(),
-		name:file.filename,
-		uploadTime:Date.now(),
-		connectedTags:[]};
+		const media: Media = {
+			id: shortid.generate(),
+			name: file.filename,
+			uploadTime: Date.now(),
+			connectedTags: []
+		};
 		app.db.AddMedia(media);
 	});
 	res.send("Done");
