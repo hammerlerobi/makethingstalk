4cec0ad4ccea4c19ef295a28d5ebd0976f408a3b
Mischka <og.mischka@gmail.com>
2020-05-08 10:50:56 +0200

    test: uploaded build for testing

---
+++ frontend/build/precache-manifest.3ef620ae47cea143d91ce90f2e711409.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876058dd552b66e280c94fb8b9cde4a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14f01c40923439d43bfc",
+    "url": "/static/css/main.ccb4bfd7.chunk.css"
+  },
+  {
+    "revision": "35779e3d8860aa5991de",
+    "url": "/static/js/2.d755b74e.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.d755b74e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14f01c40923439d43bfc",
+    "url": "/static/js/main.b952aee3.chunk.js"
+  },
+  {
+    "revision": "c71849e6b92a54d9b65d",
+    "url": "/static/js/runtime-main.dbac8e30.js"
+  },
+  {
+    "revision": "a163fafb7d7496cd0834bc3e24cb82f0",
+    "url": "/static/media/box@2x.a163fafb.png"
+  },
+  {
+    "revision": "72ee3e26eef1f487bebc1329ac97b2ae",
+    "url": "/static/media/noise.72ee3e26.png"
+  },
+  {
+    "revision": "83479c7b18afefc69216d769372da50e",
+    "url": "/static/media/player-c@2x.83479c7b.png"
+  },
+  {
+    "revision": "16b9099aadc7786aa4c1b7fea8c205ad",
+    "url": "/static/media/tag@2x.16b9099a.png"
+  }
+]);
