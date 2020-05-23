dc61d9246b47694ef66f32805ab6c58df8a52dc0
Georg Reil <georg.reil@ixds.com>
2020-05-24 00:06:56 +0200

    removed unnecessary / from thumbnail path

--- backend/src/utils/thumbnailCreator.ts
+++ backend/src/utils/thumbnailCreator.ts
@@ -3,7 +3,7 @@ import * as child from 'child_process';
 const videoPath = './uploads/'
 const tumbnailPath = videoPath + 'thumbnails/'
 const createThumbnail = (fileName:string) => {
-    child.exec('ffmpeg -i ' + videoPath + '/' + fileName + ' -ss 00:00:10.000 -vframes 1 ' + tumbnailPath + '' + fileName + '.png');
+    child.exec('ffmpeg -i ' + videoPath + fileName + ' -ss 00:00:10.000 -vframes 1 ' + tumbnailPath + fileName + '.png');
 };
 
 export {createThumbnail as createThumbnail};
