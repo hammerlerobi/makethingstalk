6d593ab7a50a4338b06e017506d08ab8c702cc9a
Georg Reil <georg.reil@ixds.com>
2020-05-31 09:53:20 +0200

    added GetAllTags method to database and PlayerRoutes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import {DataBase} from './db';
 import { MediaRoutes } from './routes/mediaRoutes';
 import {UploadRoutes} from './routes/uploadRoutes';
+import {PlayerRoutes} from './routes/playerRoutes';
 import {TagRoutes} from './routes/tagRoutes';
 import {WebsocketTransmitter} from './transmitters/websocketTransmitter';
 import {OmxPlayer} from './nativePlayer/omxPlayer';
@@ -58,6 +59,7 @@ class App {
 		this.express.use('/api/media', MediaRoutes);
 		this.express.use('/api/upload', UploadRoutes);
 		this.express.use('/api/tag', TagRoutes);
+		this.express.use('/api/player', PlayerRoutes);
 
 	}
 }
